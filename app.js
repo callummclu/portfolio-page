4524f52333c25128c99510d97c14ff69b0ceb825
Callum <callummcluskey100@gmail.com>
2022-02-19 13:56:46 +0000

    heroku setup

--- app.js
+++ app.js
@@ -33,4 +33,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/portfolio',portfolioRouter);
 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 module.exports = app;
