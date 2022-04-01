561b556f2c719ae209e1324739f4572a5d842cc9
Callum <callummcluskey100@gmail.com>
2022-04-01 17:41:40 +0100

    fixed spelling error

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -7,7 +7,7 @@ function Banner(props){
 			<Link to="../../">
 		        <div className="name-container">
 		       		<h1 className="banner-text" style={props.style}> Callum McLuskey </h1>
-		       		<p className="banner-text">Itern Full Stack Engineer @ Evata</p>
+		       		<p className="banner-text">Intern Full Stack Engineer <span style={{fontSize:"10px"}}>@</span> Evata</p>
 
 		        </div>
 			</Link>
